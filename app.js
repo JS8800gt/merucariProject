7f690704ca50476f2c2191cc26af23b72ffbf590
JS8800gt <JS8800gt@gmail.com>
2024-07-21 16:12:12 +0900

    fix(debug): 디버깅 추가

--- app.js
+++ app.js
@@ -15,28 +15,17 @@ app.post('/fetch-product-info', async (req, res) => {
         return res.status(400).json({ error: 'URL 배열을 제공해 주세요.' });
     }
 
-    console.log('Received URLs:', urls);
-
     try {
+        console.log('Received URLs:', urls);
         const results = await Promise.all(urls.map(url => getProductInfo(url)));
         console.log('Product Info Results:', results);
-
-        const missingInfo = results.some(result => result.error && result.error.includes('실패'));
-        if (missingInfo) {
-            throw new Error('상품 정보 가져오기 실패');
-        }
-
-        const totalPrice = results.reduce((total, result) => {
-            const price = parseInt(result.price.replace(',', ''), 10) || 0;
-            return total + price;
-        }, 0);
-
-        res.json({ products: results, totalPrice });
+        // ...
     } catch (error) {
         console.error('Error fetching product data:', error);
         res.status(500).json({ error: '상품 정보가 없습니다.' });
     }
 });
+
 
 
 app.listen(port, () => {
