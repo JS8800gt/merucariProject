1fc55fd6e837f896d7384d1e7243708b2cf67216
JS8800gt <JS8800gt@gmail.com>
2024-07-21 16:43:58 +0900

    fix(debug): 콘솔 추가

--- app.js
+++ app.js
@@ -15,17 +15,19 @@ app.post('/fetch-product-info', async (req, res) => {
         return res.status(400).json({ error: 'Please provide an array of URLs.' });
     }
 
+    console.log("여기까지 옴 1");
     try {
         // 병렬 처리로 속도 개선
         const results = await Promise.all(urls.map(url => getProductInfo(url)));
-
+        console.log("여기까지 옴 2");
         const totalPrice = results.reduce((total, result) => {
             const price = parseInt(result.price.replace(',', ''), 10) || 0;
             return total + price;
         }, 0);
-
+        console.log("여기까지 옴 3");
         res.json({ products: results, totalPrice });
     } catch (error) {
+        console.log("여기까지 옴 4");
         console.error('Error fetching product data:', error);
         res.status(500).json({ error: '상품 정보가 없습니다.' });
     }
